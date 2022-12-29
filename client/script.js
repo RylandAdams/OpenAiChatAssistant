dcc9e3073ec9f9e2833026876859a1c24ff8b537
Ryland Adams <rylandadams@yahoo.com>
2022-12-29 13:12:22 -0800

    front end to look at hosted server

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
 	//fetch data from the server --- bots response
 
-	const response = await fetch('http://localhost:5000', {
+	const response = await fetch('https://openai-chat-assistant.onrender.com', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
