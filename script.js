cb3b934c16d7eba12ba6b564bbbcbeca767ad083
Ryland Adams <rylandadams@yahoo.com>
2022-12-29 07:59:55 -0800

    added in functions for loading response, generating random id, and the bots text animation

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+	element.textContent = '';
+
+	loadInterval = setInterval(() => {
+		element.textContent += '.';
+
+		if (element.textContent === '....') {
+			element.textContent = '';
+		}
+	}, 300);
+}
+
+function typeText(element, text) {
+	let index = 0;
+
+	let interval = setInterval(() => {
+		if (index < text.lenght) {
+			element.innerHTML += text.chartAt(index);
+			index++;
+		} else {
+			clearInterval(interval);
+		}
+	}, 20);
+}
+
+function generateUniqueId() {
+	const timestamp = Date.now();
+	const randomNumber = Math.random();
+	const hexadecimalString = randomNumber.toString(16);
+
+	return `id-${timestamp}-${hexadecimalString}`;
+}
